1a25430836970ff72314fbf6acd814e095abffaa
balazsjarai <balazsjarai@gmail.com>
2018-08-09 13:38:58 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * futes_vezerlo.c
+ *
+ * Created: 2018.08.09. 13:36:23
+ * Author : hubaja2
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
